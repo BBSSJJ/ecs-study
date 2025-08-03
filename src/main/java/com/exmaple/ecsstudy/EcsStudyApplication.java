46b6f54bda9b126ee064fd4592ae2982e851ee43
BBSSJJ <bdhbsj@naver.com>
2025-08-03 12:37:13 +0900

    init

---
+++ src/main/java/com/exmaple/ecsstudy/EcsStudyApplication.java
@@ -0,0 +1,12 @@
+package com.exmaple.ecsstudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcsStudyApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(EcsStudyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/exmaple/ecsstudy/EcsStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.exmaple.ecsstudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcsStudyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
